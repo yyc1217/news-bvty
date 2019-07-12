e413144d980f8636b38c7eea5f6b1e8a5a01bf58
Yeh-Yung <yyc1217@gmail.com>
2019-07-12 15:19:33 +0800

    Refine fair readers code cells.

--- src/helper.py
+++ src/helper.py
@@ -1,3 +1,6 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
 def index_as_id(data_frame, suffix):
     '''讓index做為id，並獨立存在同一欄內'''
 
@@ -8,3 +11,40 @@ def index_as_id(data_frame, suffix):
     data_frame = data_frame[columns]
     
     return data_frame
+
+def draw_reporter_scores(scale, data, reporters, reporter_scores, reporter_weighted_scores):
+    '''視覺化報導者可信度'''
+
+    plt.subplot(1, 1, 1)
+    plt.xticks(np.arange(0, len(reporters), 1.0))
+
+    # 評分簡單平均計算
+    plt.plot(reporter_scores, label = 'simple avg')
+    
+    # 評分*評分者可信度的加權平均計算
+    plt.plot(reporter_weighted_scores, label = 'weighted avg')
+    
+    # 報導者可信度
+    plt.plot(reporters['reporter_score'], 'k--', label = 'origin', c = '0.55')
+
+    plt.ylim(scale.min, scale.max)
+    plt.xlabel("Reporters")
+    plt.ylabel("Scores")
+    plt.title("Believability of Reporters")
+    plt.legend(loc = 'upper right')
+    plt.show()
+
+def draw_user_weights(scale, weights):
+    weights = list(weights)
+
+    origin = list([scale.mean]) * len(weights)
+    
+    plt.plot(weights)
+    plt.plot(origin, 'k--', label = 'real', c = '0.55')
+
+    plt.xticks(np.arange(0, len(weights), 1.0))
+    plt.ylim(scale.min, scale.max)
+    plt.ylabel("Weights")
+    plt.xlabel("Readers")
+    plt.title("Weights of Readers")
+    plt.show()
